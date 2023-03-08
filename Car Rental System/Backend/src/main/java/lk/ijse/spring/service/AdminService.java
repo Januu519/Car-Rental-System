91bf0f81496788f88d3a2a1c6ea672d85d3314b3
unknown <jayaminisakunthala629@gmail.com>
2023-03-08 20:44:41 +0530

    Content updated

---
+++ Car Rental System/Backend/src/main/java/lk/ijse/spring/service/AdminService.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.AdminDTO;
+
+import java.util.ArrayList;
+
+public interface AdminService {
+    public void addAdmin(AdminDTO dto);
+
+    public void deleteAdmin(String id);
+
+    public void updateAdmin(AdminDTO dto);
+
+    public ArrayList<AdminDTO> getAllAdmins();
+
+}
